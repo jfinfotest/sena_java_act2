463dbfa6c6cedcecae83a85166bbb7b3128f861c
Jhon <jfinfocesde@gmail.com>
2026-05-08 09:37:25 -0500

    Variables

--- src/main/java/com/example/VariablesYConstantes.java
+++ src/main/java/com/example/VariablesYConstantes.java
@@ -3,6 +3,8 @@ package com.example;
 public class VariablesYConstantes {
     public static void demostrarDeclaracionAsignacion() {        
         // TODO
+        int edad = 10;
+        System.out.println(edad);
     }
 
     public static void demostrarConvencionesNombres() {
